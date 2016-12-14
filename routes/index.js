f7836255e1d26df325e5112b5af5db5e25371745
Chana Reynolds <chana777@gmail.com>
2016-12-14 10:45:40 -0700

    New task on detail page

--- routes/index.js
+++ routes/index.js
@@ -2,66 +2,47 @@
 
 var express = require('express');
 var router = express.Router();
-const knex= require('../db/knex');
+const knex = require('../db/knex');
 const env = 'development';
 
 
 
-/* GET home page. */
+// Gets home page with seed data
 router.get('/', function(req, res, next) {
-  knex('list').select().then(function(lists){
-    res.render('index',{lists:lists});
-  });
-});
-
-// new form to add stuff
-router.get('/new', function(req, res, next) {
-  res.render('new', { title: 'stuff' });
-});
-//
-
-router.post('/', function(req, res, next) {
-  knex('list')
-  .insert(req.body.id)
-  .returning('id')
-  .then(function (id) {
-    res.redirect('/detail/'+id);
-  });
-  });
-
-  router.get('/detail/:id', function(req, res, next){
-    knex('list')
-    .where('id',req.params.id)
-    .then(function(task) {
-      res.render('detail', task[0])
-    })
-  })
-
-  router.get('/', function(req, res, next) {
-    knex('list')
-    .where('id',req.params.id)
-    .first()
-    .then(data)
-    res.render('list_id', {
-      name: list.name
+    knex('list').select().then(function(lists) {
+        res.render('index', {
+            lists: lists
+        });
     });
-  });
-// posting from form
-router.post('/new', function(req, res, next) {
-  knex('list')
-  .insert(req.body)
-  .then(function(){
-    // res.redirect('/');
-  })
-  console.log(req.body);
-  });
+});
+
+// New form to add task
+router.get('/new', function(req, res, next) {
+    res.render('new', {
+        title: 'stuff'
+    });
+});
 //
-// router.get('/list', (req, res, next) => {
-//   knex('list')
-//   .select()
-//   .then(list);
-// });
-//
-//
+//  Gets detail view of one task
+router.get('/detail/:id', function(req, res, next) {
+    knex('list')
+        .where('id', req.params.id)
+        .then(function(task) {
+            res.render('detail', task[0])
+        })
+})
+
+// creates a new task and renders new task on detail page
+router.post('/new', (req, res, next) => {
+    knex('list')
+        .insert(req.body)
+        .returning('id')
+        .then(function(id) {
+            res.redirect('/detail/' + id);
+        });
+});
+
+
+
 
 module.exports = router;
--- seeds/todo.js
+++ seeds/todo.js
@@ -6,56 +6,48 @@ exports.seed = function(knex, Promise) {
       var listSeed = [
 
 	knex('list').insert({
-		id: 1,
 		name: 'Chana',
 		date: '12/13/2016',
 		time: '1pm',
     event:'Make fullstack app'
 	}),
   knex('list').insert({
-    id: 2,
     name: 'Ryder',
     date: '12/14/2016',
     time: '12pm',
     event:'Do dishes'
   }),
   knex('list').insert({
-    id: 3,
     name: 'Ryder',
     date: '12/17/2016',
     time: '3pm',
     event:'Make dinner'
   }),
   knex('list').insert({
-    id: 4,
     name: 'Chana',
     date: '12/17/2016',
     time: '4pm',
     event:'Study'
   }),
   knex('list').insert({
-    id: 5,
     name: 'Chana',
     date: '12/19/2016',
     time: '6pm',
     event:'Clean'
   }),
   knex('list').insert({
-    id: 6,
     name: 'Chana',
     date: '12/20/2016',
     time: '7pm',
     event:'Study'
   }),
   knex('list').insert({
-    id: 7,
     name: 'Ryder',
     date: '12/20/2016',
     time: '6pm',
     event:'Work'
   }),
   knex('list').insert({
-    id: 8,
     name: 'Chana',
     date: '12/20/2016',
     time: '5pm',
